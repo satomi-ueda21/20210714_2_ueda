f045b10741b32694ac9362fc29993c44220276e6
satomi-ueda21 <gionkuri@hotmail.com>
2021-07-14 11:02:06 +0900

    message

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -46,16 +46,7 @@ class TodoController extends Controller
      */
     public function show(Todo $todo)
     {
-        $item = Todo::find($todo);
-        if ($item) {
-            return response()->json([
-                'data' => $item
-            ], 200);
-        } else {
-            return response()->json([
-                'message' => 'Not found',
-            ], 404);
-        }
+       //
     }
 
     /**
